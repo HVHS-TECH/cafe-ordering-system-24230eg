08d50d71c1a7437bc6a1eb91857edc873b7067fc
24230 <24230eg@hvhs.school.nz>
2026-06-09 00:30:56 +0000

    startedmenue

--- script.js
+++ script.js
@@ -1,1 +1,116 @@
-console.log("Hello world!")
+console.log("Running");
+
+/********
+Varibles:
+********/
+
+//Items
+
+let cookieMenu;
+
+cookieMenu = [
+    {
+        name:"plain cookie",
+        price:"0.1"
+    },
+    {
+        name:"chocolate chip cookie",
+        price:"0.2"
+    },
+    {
+        name:"double chocolate chip cookie",
+        price:"0.4"
+    },
+    {
+        name:"triple chocolate chip cookie",
+        price:"0.8"
+    },
+    {
+        name:"quadruple chocolate chip cookie",
+        price:"1.6"
+    },
+    {
+        name:"quintuple chocolate chip cookie",
+        price:"3.2"
+    },
+    {
+        name:"sextuple chocolate chip cookie",
+        price:"6.4"
+    },
+    {
+        name:"septuple chocolate chip cookie",
+        price:"12.8"
+    },
+    {
+        name:"octuple chocolate chip cookie",
+        price:"25.6"
+    },
+    {
+        name:"nonuple chocolate chip cookie",
+        price:"51.2"
+    },
+    {
+        name:"decuple chocolate chip cookie",
+        price:"102.4"
+    },
+    {
+        name:"dark chocolate chip cookie",
+        price:"0.3"
+    },
+    {
+        name:"white chocolate chip cookie",
+        price:"0.3"
+    },
+    {
+        name:"dark chocolate cookie with choclate chips",
+        price:"0.4"
+    },
+    {
+        name:"dark chocolate cookie with white choclate chips",
+        price:"0.4"
+    },
+    {
+        name:"white chocolate cookie with white choclate chips",
+        price:"0.4"
+    },
+    {
+        name:"chocolate chunk cookie",
+        price:"0.3"
+    },
+    {
+        name:"double chocolate chunk cookie",
+        price:"0.6"
+    },
+    {
+        name:"macadamia nut cookie",
+        price:"0.5"
+    },
+    {
+        name:"macadamia nut and white chocolate chip cookie",
+        price:"0.7"
+    },
+    {
+        name:"oatmeal raisin cookie",
+        price:"0.4"
+    },
+    {
+        name:"cranberry cookie",
+        price:"0.5"
+    },
+    {
+        name:"cranberry and white chocolate chip cookie",
+        price:"0.7"
+    },
+    {
+        name:"peanut butter cookie",
+        price:"0.3"
+    },
+    {
+        name:"peanut butter and choclate chip cookies",
+        price:"0.5"
+    },
+    {
+        name:"walnut cookie",
+        price:"0.4"
+    },
+];

ad887d363eabb43d02caa4398bf38805e5fb7a23
24230 <24230eg@hvhs.school.nz>
2026-06-10 02:35:33 +0000

    Jun 10, 2026, 2:35 AM

--- script.js
+++ script.js
@@ -11,152 +11,157 @@ let cookieMenu;
 cookieMenu = [
     {
         name:"plain cookie",
-        price:"0.1"
+        price:0.1
     },
     {
         name:"chocolate chip cookie",
-        price:"0.2"
+        price:0.2
     },
     {
         name:"double chocolate chip cookie",
-        price:"0.4"
+        price:0.4
     },
     {
         name:"triple chocolate chip cookie",
-        price:"0.8"
+        price:0.8
     },
     {
         name:"quadruple chocolate chip cookie",
-        price:"1.6"
+        price:1.6
     },
     {
         name:"quintuple chocolate chip cookie",
-        price:"3.2"
+        price:3.2
     },
     {
         name:"sextuple chocolate chip cookie",
-        price:"6.4"
+        price:6.4
     },
     {
         name:"septuple chocolate chip cookie",
-        price:"12.8"
+        price:12.8
     },
     {
         name:"octuple chocolate chip cookie",
-        price:"25.6"
+        price:25.6
     },
     {
         name:"nonuple chocolate chip cookie",
-        price:"51.2"
+        price:51.2
     },
     {
         name:"decuple chocolate chip cookie",
-        price:"102.4"
+        price:102.4
     },
     {
         name:"dark chocolate chip cookie",
-        price:"0.3"
+        price:0.3
     },
     {
         name:"white chocolate chip cookie",
-        price:"0.3"
+        price:0.3
     },
     {
         name:"dark chocolate cookie with choclate chips",
-        price:"0.4"
+        price:0.4
     },
     {
         name:"dark chocolate cookie with white choclate chips",
-        price:"0.4"
+        price:0.4
     },
     {
         name:"white chocolate cookie with white choclate chips",
-        price:"0.4"
+        price:0.4
     },
     {
         name:"chocolate chunk cookie",
-        price:"0.3"
+        price:0.3
     },
     {
         name:"double chocolate chunk cookie",
-        price:"0.6"
+        price:0.6
     },
     {
         name:"macadamia nut cookie",
-        price:"0.5"
+        price:0.5
     },
     {
         name:"macadamia nut and white chocolate chip cookie",
-        price:"0.7"
+        price:0.7
     },
     {
         name:"oatmeal raisin cookie",
-        price:"0.4"
+        price:0.4
     },
     {
         name:"cranberry cookie",
-        price:"0.5"
+        price:0.5
     },
     {
         name:"cranberry and white chocolate chip cookie",
-        price:"0.75"
+        price:0.75
     },
     {
         name:"peanut butter cookie",
-        price:"0.3"
+        price:0.3
     },
     {
         name:"peanut butter and choclate chip cookie",
-        price:"0.5"
+        price:0.5
     },
     {
         name:"walnut cookie",
-        price:"0.45"
+        price:0.45
     },
     {
         name:"walnut and choclate chip cookie",
-        price:"0.6"
+        price:0.6
     },
     {
         name:"monster cookie",
-        price:"0.8"
+        price:0.8
     },
     {
         name:"cookies and cream cookie",
-        price:"0.5"
+        price:0.5
     },
     {
         name:"red velvet cookie",
-        price:"0.7"
+        price:0.7
     },
     {
         name:"eclipse cookie",
-        price:"0.3"
+        price:0.3
     },
     {
         name:"ginger bread cookie",
-        price:"0.9"
+        price:0.9
     },
     {
         name:"walnut cookie",
-        price:"0.4"
+        price:0.4
     },
     {
         name:"cookie cookie",
-        price:"1"
+        price:1
     }, 
     {
         name:"cookie dough",
-        price:"2.5"
+        price:2.5
     },
     {
         name:"mini belgium biscuit",
-        price:"0.25"
+        price:0.25
     },
     {
         name:"belgium biscuit",
-        price:"0.9"
+        price:0.9
     },
 ];
-/
-to add a new item
+/********************************************************************************
+to add a new item add this to the end of the array with the name and price of the item changed
+    {
+        name:"cookie name",
+        price:(cookie price)
+    },
+********************************************************************************/

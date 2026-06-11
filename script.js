c7fbce1c1e674b22e8c2b5eb030af692d6f02c01
24230 <24230eg@hvhs.school.nz>
2026-06-11 21:40:58 +0000

    Jun 11, 2026, 9:40 PM

--- script.js
+++ script.js
@@ -190,7 +190,7 @@ BODY.style.backgroundColor = "#0b5394";
 
 BODY.innerHTML="<div id='titleDiv' class='header'><h1>Ethan's bakery</h1></div>"
 
-BODY.innerHTML+="<div><form onsubmit='return false;' id='nameForm'><label for='nameInput'>Order name:</label><input type='text' id='nameInput' name='nameInput'><br><input type='submit' onclick='nameSubmit()'></form></div>"
+BODY.innerHTML+="<div class='nameform'><form onsubmit='return false;' id='nameForm'><label for='nameInput'>Order name:</label><input type='text' id='nameInput' name='nameInput'><br><input type='submit' onclick='nameSubmit()'></form></div>"
 
 function nameSubmit(){
     

5961839ef189c732c456959efc4972d753f129c3
24230 <24230eg@hvhs.school.nz>
2026-06-18 21:32:58 +0000

    Jun 18, 2026, 9:32 PM

--- script.js
+++ script.js
@@ -224,7 +224,7 @@ function displayMenu(){
     for(let i = 0; i < COOKIEMENU.length; i = (i+1)){
         currentCookie = COOKIEMENU[i];
         console.log(currentCookie.name);
-        BODY.innerHTML += "<div class='menuitem'><img src='cookies/" + i + ".png' alt='A picture of a " + currentCookie.name + ".'></img><h2>" + currentCookie.name + "</h2></div>";
+        BODY.innerHTML += "<div class='menuitem' id='" + i +"'><img src='cookies/" + i + ".png' alt='A picture of a " + currentCookie.name + ".'></img><h2>" + currentCookie.name + "</h2></div>";
     };
 };
 
@@ -239,5 +239,5 @@ function displayNameForm(){
 };
 
 function chooseNumberOfCookies(_i){
-    
+
 };

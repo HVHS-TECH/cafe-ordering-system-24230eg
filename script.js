51285a6992f096f1bea289c07efc981073fada8d
24230 <24230eg@hvhs.school.nz>
2026-06-24 03:02:36 +0000

    Jun 24, 2026, 3:02 AM

--- script.js
+++ script.js
@@ -295,5 +295,5 @@ function displayCart(){
         console.log("bor");
         document.getElementById("fancyCartDiv").innerHTML += "<p>" + userCart[i].amount + " - "+ COOKIEMENU[userCart[i].item].name + "'s</p>"
     };
-    document.getElementById("fancyCartDiv").innerHTML += "<
+    document.getElementById("fancyCartDiv").innerHTML += "<button>back</button>"
 };
